982ddfd28ad3f0a6ffe36d2654c91fc90e641902
KaiDung <allen01105@gmail.com>
2020-10-25 17:38:44 +0800

    modify search tab

--- php/record.php
+++ php/record.php
@@ -4,8 +4,14 @@ $db_username = "root";
 $db_password = "root";
 $db_dbname = "facial_recognition";
 
+
 $user_name = $_POST["user_name"];
-$time = $_POST["time"];
+$year = $_POST["year"];
+$month= $_POST["month"];
+$day  = $_POST["day"];
+$hour = $_POST["hour"];
+$min = $_POST["min"];
+$sec = $_POST["sec"];
 $mask = $_POST["mask"];
 
 // Create connection
@@ -15,7 +21,7 @@ $conn = new mysqli($db_servername, $db_username, $db_password,$db_dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql ="INSERT INTO search(user_name,time) VALUES ('$user_name','$time','$mask')";
+$sql ="INSERT INTO search(user_name,year,month,day,hour,min,sec,mask) VALUES ('$user_name','$year','$month','$day','$hour','$min','$sec','$mask')";
 
 $result = $conn ->query($sql);
 
--- php/search.php
+++ php/search.php
@@ -6,20 +6,52 @@ $db_dbname = "facial_recognition";
 
 
 $user_name = $_POST["user_name"];
+$cb = $_POST["cb"];
+$cb2 = $_POST["cb2"];
 
 $conn = new mysqli($db_servername, $db_username, $db_password,$db_dbname);
 
-$sql ="SELECT * FROM `search` WHERE `user_name` = '$user_name' ORDER BY `number` DESC LIMIT 10";
+$year = date("Y");
+$month = date("m");
+$day = date("d");
 
+$sql ="SELECT * FROM `search`";
+
+//判斷comboBox選項
+if($cb == "今天"){
+	$sql = $sql." WHERE `day` = '$day' ";
+}
+else if($cb == "本週"){
+	$day2=$day - 7;
+	$sql = $sql." WHERE `month` = $month AND `day` <= $day AND `day` >= $day2  ";
+}
+else if($cb == "本月"){
+	$sql = $sql." WHERE `month`= $month ";
+}
+//判斷comboBox_2選項
+if($cb2 == "只顯示沒戴好"){
+	$sql = $sql." AND (`mask` = 'none' OR `mask` = 'bad') ";
+}
+
+//判斷有沒有輸入名字
+if($user_name != ""){
+	$sql = $sql." AND `user_name` = '$user_name'";
+}
+//echo $sql
+$json = array();
 $result = $conn ->query($sql);
-
 while($row = $result->fetch_array())
 {
 	$obj = array("user_name" => $row["user_name"],
-				"time" => $row["time"],
+				"year" => $row["year"],
+				"month" => $row["month"],
+				"day" => $row["day"],
+				"hour" => $row["hour"],
+				"min" => $row["min"],
+				"sec" => $row["sec"],
 				"mask" => $row["mask"],
 				);
-	echo json_encode($obj);
+	array_push($json,$obj);
 }
-
+echo json_encode($json);
 ?>
